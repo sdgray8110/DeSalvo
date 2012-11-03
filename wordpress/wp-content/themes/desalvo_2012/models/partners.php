9109d24755f55dfb962dc5ed9051af8becb5d3a9
Spencer Gray <spencer@mapletonhillmedia.com>
2012-11-03 12:11:24 -0600

    updated database - changes to about, partners and order pages

--- wordpress/wp-content/themes/desalvo_2012/models/partners.php
+++ wordpress/wp-content/themes/desalvo_2012/models/partners.php
@@ -1,6 +1,6 @@
 <?php
 
-class Generic {
+class Partners {
     protected $id;
     private $post;
 
@@ -9,10 +9,15 @@ class Generic {
         $this->title = $this->post->post_title;
         $this->content = $this->post->post_content;
         $this->name = $this->post->post_name;
+        $this->partners = $this->get_partners();
     }
 
     private function get_post() {
         $this->id = get_the_ID();
         $this->post =  get_post($this->id);
+    }
+
+    private function get_partners() {
+        return ac_get_field('partners', $this->id);
     }
 }
